eba2963f4997f658d06a6340c87e06cf8d30a034
maomao2023 <2438708917@qq.com>
2024-11-28 21:34:21 +0800

    sinh

---
+++ SinhCustom/op_host/sinh_custom_tiling.h
@@ -0,0 +1,14 @@
+#ifndef Sinh_CUSTOM_TILING_H
+#define Sinh_CUSTOM_TILING_H
+#include "register/tilingdata_base.h"
+
+namespace optiling {
+BEGIN_TILING_DATA_DEF(SinhCustomTilingData)
+  //考生自行定义tiling结构体成员变量
+  TILING_DATA_FILED_DEF(uint32_t,totalLength);
+  TILING_DATA_FILED_DEF(uint32_t,tileNum);
+END_TILING_DATA_DEF;
+
+REGISTER_TILING_DATA_CLASS(SinhCustom, SinhCustomTilingData)
+}
+#endif
