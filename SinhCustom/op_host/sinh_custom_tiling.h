6fb3b7c5adffb5b7d87fb3faa2d840ff286149ce
maomao2023 <2438708917@qq.com>
2024-11-29 15:53:33 +0800

    sinh

--- SinhCustom/op_host/sinh_custom_tiling.h
+++ SinhCustom/op_host/sinh_custom_tiling.h
@@ -5,8 +5,8 @@
 namespace optiling {
 BEGIN_TILING_DATA_DEF(SinhCustomTilingData)
   //考生自行定义tiling结构体成员变量
-  TILING_DATA_FILED_DEF(uint32_t,totalLength);
-  TILING_DATA_FILED_DEF(uint32_t,tileNum);
+  TILING_DATA_FIELD_DEF(uint32_t,totalLength);
+  TILING_DATA_FIELD_DEF(uint32_t,tileNum);
 END_TILING_DATA_DEF;
 
 REGISTER_TILING_DATA_CLASS(SinhCustom, SinhCustomTilingData)
