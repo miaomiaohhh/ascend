52028c9a59b57be264d39128a0e5b5f2a280735b
maomao2023 <2438708917@qq.com>
2024-11-30 09:10:05 +0800

    addsinh

---
+++ AddKernelInvocation/add_custom.cpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2023. All rights reserved.
+ *
+ * Function : z = x + y
+ * This sample is a very basic sample that implements vector add on Ascend plaform.
+ */
+#include "kernel_operator.h"
+using namespace AscendC;
+
+constexpr int32_t TOTAL_LENGTH = 8 * 2048;                            // total length of data
+constexpr int32_t USE_CORE_NUM = 8;                                   // num of core used
+constexpr int32_t BLOCK_LENGTH = TOTAL_LENGTH / USE_CORE_NUM;         // length computed of each core
+constexpr int32_t TILE_NUM = 8;                                       // split data into 8 tiles for each core
+constexpr int32_t BUFFER_NUM = 2;                                     // tensor num for each queue
+constexpr int32_t TILE_LENGTH = BLOCK_LENGTH / TILE_NUM / BUFFER_NUM; // seperate to 2 parts, due to double buffer
+
+class KernelSinh {
+public:
+    __aicore__ inline KernelSinh() {}
+    __aicore__ inline void Init(GM_ADDR x, GM_ADDR y)
+    {
+        xGm.SetGlobalBuffer((__gm__ half*)x + BLOCK_LENGTH * GetBlockIdx(), BLOCK_LENGTH);
+        yGm.SetGlobalBuffer((__gm__ half*)y + BLOCK_LENGTH * GetBlockIdx(), BLOCK_LENGTH);
+        pipe.InitBuffer(inQueueX, BUFFER_NUM, TILE_LENGTH * sizeof(half));
+        pipe.InitBuffer(inQueueY, BUFFER_NUM, TILE_LENGTH * sizeof(half));
+    }
+    __aicore__ inline void Process()
+    {
+        int32_t loopCount = TILE_NUM * BUFFER_NUM;
+        for (int32_t i = 0; i < loopCount; i++) {
+            CopyIn(i);
+            Compute(i);
+            CopyOut(i);
+        }
+    }
+
+private:
+    __aicore__ inline void CopyIn(int32_t progress)
+    {
+        LocalTensor<half> xLocal = inQueueX.AllocTensor<half>();
+        DataCopy(xLocal, xGm[progress * TILE_LENGTH], TILE_LENGTH);
+        inQueueX.EnQue(xLocal);
+    }
+    __aicore__ inline void Compute(int32_t progress)
+    {
+        LocalTensor<half> xLocal = inQueueX.DeQue<half>();
+        LocalTensor<half> yLocal = outQueueY.AllocTensor<half>();
+        Exp(xLocal, xLocal, TILE_LENGTH);
+        Reciprocal(yLocal,xLocal,TILE_LENGTH);
+        Sub(yLocal,xLocal,yLocal,TILE_LENGTH);
+        half scalar = 0.5;
+        Muls(yLocal,yLocal,scalar,TILE_LENGTH);
+        outQueueY.EnQue<half>(yLocal);
+        inQueueX.FreeTensor(xLocal);
+    }
+    __aicore__ inline void CopyOut(int32_t progress)
+    {
+        LocalTensor<half> yLocal = outQueueY.DeQue<half>();
+        DataCopy(yGm[progress * TILE_LENGTH], yLocal, TILE_LENGTH);
+        outQueueY.FreeTensor(yLocal);
+    }
+
+private:
+    TPipe pipe;
+    TQue<QuePosition::VECIN, BUFFER_NUM> inQueueX;
+    TQue<QuePosition::VECOUT, BUFFER_NUM> outQueueY;
+    GlobalTensor<half> xGm;
+    GlobalTensor<half> yGm;
+};
+
+extern "C" __global__ __aicore__ void sinh_custom(GM_ADDR x, GM_ADDR y)
+{
+    KernelSinh op;
+    op.Init(x, y);
+    op.Process();
+}
+
+#ifndef __CCE_KT_TEST__
+// call of kernel function
+void sinh_custom_do(uint32_t blockDim, void* l2ctrl, void* stream, uint8_t* x, uint8_t* y)
+{
+    sinh_custom<<<blockDim, l2ctrl, stream>>>(x, y);
+}
+#endif
